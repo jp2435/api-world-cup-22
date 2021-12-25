ff9d80ff610d2a558bf5a5b0e518ca8b64e1c40b
jp2435 <fuseta00@gmail.com>
2021-12-25 23:13:17 +0000

    Registration and authentication route completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,9 +5,62 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
 const authConfig = require('../config/auth');
+const User = require( '../models/user');
 
-router.get('/', async(req,res) => {
-    console.log('Route /auth its working');
+function generateToken(params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 5184000
+    });
+};
+
+router.post('/register', async(req,res) => {
+    const { email, AcessCode } = req.body
+    try{
+        if(AcessCode!=authConfig.AcessCode){
+            return res.status(401).send({
+                error: 'Unauthorized to register, contact an admin'
+            });
+        };
+        if(await User.findOne({ email })){
+            return res.status(400).send({
+                error: 'User already exists'
+            });
+        };
+        const user = await User.create(req.body);
+        user.password = undefined; // To dont show the password
+
+        return res.send({
+            user,
+            token: generateToken({id: user.id})
+        });
+
+    }catch(err){
+        return res.status(400).send({
+            error: 'Registration failed'
+        });
+    };
+});
+
+router.post('/authenticate', async(req,res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(400).send({
+            error: 'User not found'
+        });
+    };
+    if(!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({
+            error: 'Invalid password'
+        });
+    };
+    user.password = undefined;
+
+    res.send({
+        user,
+        token: generateToken({id:user.id})
+    });
 });
 
 module.exports = app => app.use('/auth', router);
