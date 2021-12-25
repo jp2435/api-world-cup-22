c354bfe0ef99cb886d2da174fd0a2de2ac0f2486
jp2435 <fuseta00@gmail.com>
2021-12-25 00:49:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+const PORT = process.env.PORT || 2435;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>{
+    console.log(`Server started on port ${PORT}`);
+});
