57df5ce7a5fdffb7eb00ac8611981bba93ff8727
jp2435 <fuseta00@gmail.com>
2021-12-28 00:32:18 +0000

    Data Controller created

---
+++ controllers/dataController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const authConfig = require('../config/auth');
+
+const Group = require('../models/group');
+const Team = require('../models/team');
+const Game = require('../models/game');
+
+router.use(authMiddleware);
+
+module.exports = app => app.use('/data', router);
