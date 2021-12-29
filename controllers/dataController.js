f4b215a5dc4a1eced635e17aa0f05e04f71b6951
jp2435 <fuseta00@gmail.com>
2021-12-29 13:27:28 +0000

    Team routes created

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -22,8 +22,7 @@ router.post('/group', async(req,res) => {
         return res.send({ group });
     }catch(err){
         return res.status(400).send({
-            error: 'Error creating group',
-            log: err
+            error: 'Error creating group'
         });
     };
 });
@@ -39,8 +38,7 @@ router.put('/group/:idGroup', async(req,res) => {
     }catch(err){
         console.log(err)
         return res.status(400).send({
-            error: 'Error editing group',
-            log: err
+            error: 'Error editing group'
         });
     };
 });
@@ -60,7 +58,61 @@ router.delete('/group/:idGroup', async(req,res) => {
             error: 'Error deleting group'
         })
     }
-})
+});
 
+
+// Team Data
+
+router.post('/team', async(req,res) => {
+    try{
+        const { name, group, AcessCodeDev } = req.body;
+        if(!(AcessCodeDev==authConfig.AcessCodeDev)){
+            return res.status(401).send({error: 'Unauthorized to create team'});
+        };
+        if(await Team.findOne({name})){
+            return res.status(400).send({error : 'Team already exist'});
+        };
+        const groupName = await Group.findOne({name: group});
+        const team = await Team.create({...req.body, group: groupName._id});
+
+        return res.send({team});
+    }catch(err){
+        return res.status(400).send({
+            error: 'Error creating team'
+        });
+    };
+});
+
+router.put('/team/:idTeam', async(req,res) => {
+    try{
+        const { AcessCodeDev } = req.body;
+        if(!(AcessCodeDev==authConfig.AcessCodeDev)){
+            return res.status(401).send({error: 'Unauthorized to edit team'});
+        };
+        const team = await Team.findByIdAndUpdate(req.params.idTeam, {...req.body}, {new: true}).populate('group');
+        
+        return res.send({team});
+    }catch(err){
+        return res.status(400).send({
+            error: 'Error editing team'
+        });
+    };
+});
+
+router.delete('/team/:idTeam', async(req,res) => {
+    try{
+        const { AcessCodeDev } = req.body;
+        if(!(AcessCodeDev==authConfig.AcessCodeDev)){
+            return res.status(401).send({error: 'Unauthorized to deleye team'});
+        };
+        await Team.findByIdAndDelete(req.params.idTeam);
+
+        return res.send({ok: true});
+    }catch(err){
+        return res.send({
+            error: 'Error deleting team'
+        });
+    };
+});
 
 module.exports = app => app.use('/data', router);
