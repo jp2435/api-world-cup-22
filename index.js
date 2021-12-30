b5c770c0ed700209920e8c548357885be5622062
jp2435 <fuseta00@gmail.com>
2021-12-30 23:26:56 +0000

    Added route not found answer

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.use(bodyParser.json());
 
 require('./controllers/index')(app);
 
+app.use((req, res, next) => {
+    const erro = new Error('Route not found');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        error: {
+            message: error.message
+        }
+    })
+});
+
 app.listen(PORT, () =>{
     console.log(`Server started on port ${PORT}`);
 });
