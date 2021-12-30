ccff8cd4b3d78b249dee7a86db1d5e457430223d
jp2435 <fuseta00@gmail.com>
2021-12-30 00:51:30 +0000

    Group get routes created

--- controllers/getController.js
+++ controllers/getController.js
@@ -9,4 +9,48 @@ const Game = require('../models/game');
 router.use(authMiddleware);
 
 
+// Group Get Routes
+
+router.get('/group/:idGroup', async(req,res) => {
+    try{
+        const group = await Group.findById(req.params.idGroup);
+        const teams = await Team.find({group: group._id});
+
+        return res.send({group, teams});
+    }catch(err){
+        return res.status(400).send({
+            error: `Error getting group with id:${req.params.idGroup}`
+        });
+    };
+});
+
+router.get('/group', async(req,res) => {
+    try{
+        if(req.query.name == undefined){
+            return res.status(400).send({
+                error: 'Error no query declared'
+            });
+        };
+        const group = await Group.findOne({name: req.query.name});
+
+        return res.send({group});
+    }catch(err){
+        return res.status(400).send({
+            error: 'Eror'
+        })
+    };
+});
+
+router.get('/groups', async(req,res) => {
+    try{
+        const groups = await Group.find();
+
+        return res.send({groups});
+    }catch(err){
+        return res.status(400).send({
+            error: 'Error gettings all groups'
+        });
+    };
+});
+
 module.exports = app => app.use('/get', router);
