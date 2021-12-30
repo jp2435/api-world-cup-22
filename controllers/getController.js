9e906940e80a4832d4b31c911d98f33237ef9e58
jp2435 <fuseta00@gmail.com>
2021-12-30 22:54:06 +0000

    Team get routes created

--- controllers/getController.js
+++ controllers/getController.js
@@ -8,7 +8,6 @@ const Game = require('../models/game');
 
 router.use(authMiddleware);
 
-
 // Group Get Routes
 
 router.get('/group/:idGroup', async(req,res) => {
@@ -50,6 +49,57 @@ router.get('/groups', async(req,res) => {
     }catch(err){
         return res.status(400).send({
             error: 'Error getting all groups'
+        });
+    };
+});
+
+
+// Team Get Routes
+
+router.get('/team/:idTeam', async(req,res) => {
+    try{
+        const team = await Team.findById(req.params.idTeam).populate(['group', 'games']);
+
+        return res.send({team});
+    }catch(err){
+        return res.send.status(400).send({
+            error: `Error getting team with id:${req.params.idTeam}`
+        });
+    };
+});
+
+router.get('/team', async(req,res) => {
+    try{
+        if(req.query.name == undefined){
+            return res.status(400).send({
+                error: 'Error no query declared'
+            });
+        };
+        const name = req.query.name.charAt(0).toUpperCase() + req.query.name.slice(1);
+        const team = await Team.findOne({name: name}).populate(['group', 'games']);
+
+        return res.send({team});
+    }catch(err){
+        return res.status(400).send({
+            error: `Error getting team with name: ${req.query.name}`
+        });
+    };
+});
+
+router.get('/teams', async(req,res) => {
+    try{
+        if(!(req.query.group == undefined)){
+            const group = await Group.findOne({name: req.query.group});
+            const teams = await Team.find({group: group._id});
+            
+            return res.send({teams, group});
+        };
+        const teams = await Team.find().populate('group');
+
+        return res.send({teams});
+    }catch(err){
+        return res.status(400).send({
+            error: 'Error getting all teams'
         });
     };
 });
